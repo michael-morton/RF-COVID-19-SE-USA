7d7f32b02cb56380d464b173e631194c4330a6bf
michael-morton <55459635+michael-morton@users.noreply.github.com>
2021-06-08 17:00:32 -0400

    Add files via upload

---
+++ SouthEast Counties/Cases (Area Removed)/SE_Cases_Area_Removed.py
@@ -0,0 +1,55 @@
+import pandas as pd
+import numpy as np
+
+df = pd.read_csv("SE_Counties.csv")
+#print(df.head())
+#print(df.dtypes)
+print(df.describe())
+
+df = df.select_dtypes(exclude=['object'])
+#df = df.fillna(df.mean())
+
+X = df.drop(['cases', 'area (miles^2)'],axis=1)
+y = df['cases']
+
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
+
+test = X_test
+#test.to_csv('test.csv', index = False)
+
+from sklearn.ensemble import RandomForestRegressor
+regressor = RandomForestRegressor(n_estimators = 1000, random_state = 42)
+regressor.fit(X_train, y_train)
+
+y_pred = regressor.predict(X_test)
+df = pd.DataFrame({'actual cases':y_test, 'predicted cases':y_pred})
+
+importance = regressor.feature_importances_
+
+print()
+for i,v in enumerate(importance):
+	print('Feature: %0d, Score: %.5f' % (i,v))
+
+results = df
+#results.to_csv('results_cases_area_removed.csv', index = False)
+
+concat_results = pd.concat([test, results], axis = 1)
+concat_results.to_csv('results_cases.csv', index = False)
+
+from sklearn import metrics
+print()
+print('Explained Variance Score:' , metrics.explained_variance_score(y_test, y_pred))
+print('R^2 Score:' , metrics.r2_score(y_test, y_pred))
+
+# Calculate the absolute errors
+errors = abs(y_pred - y_test)
+# Print out the mean absolute error (mae)
+print('Mean Absolute Error:', round(np.mean(errors), 2), 'degrees.')
+
+# Calculate mean absolute percentage error (MAPE)
+mape = 100 * (errors / y_test)
+# Calculate and display accuracy
+accuracy = 100 - np.mean(mape)
+print('Accuracy:', round(accuracy, 2), '%.')
+print()
---
+++ SouthEast States/Total Death (Total Cases Removed)/States_Death_TC_Removed.py
@@ -0,0 +1,51 @@
+import pandas as pd
+import numpy as np
+
+df = pd.read_csv("SE_States.csv")
+#print(df.head())
+#print(df.dtypes)
+print(df.describe())
+
+df = df.select_dtypes(exclude=['object'])
+#df = df.fillna(df.mean())
+
+X = df.drop(['tot_death', 'tot_cases'] ,axis=1)
+y = df['tot_death']
+
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
+
+test = X_test
+#test.to_csv('test.csv', index = False)
+
+from sklearn.ensemble import RandomForestRegressor
+regressor = RandomForestRegressor(n_estimators = 1000, random_state = 42)
+regressor.fit(X_train, y_train)
+
+y_pred = regressor.predict(X_test)
+df = pd.DataFrame({'actual cases':y_test, 'predicted cases':y_pred})
+
+importance = regressor.feature_importances_
+
+print()
+for i,v in enumerate(importance):
+	print('Feature: %0d, Score: %.5f' % (i,v))
+
+results = df
+#results.to_csv('results_cases.csv', index = False)
+
+concat_results = pd.concat([test, results], axis = 1)
+concat_results.to_csv('results_tot_death_tc_removed.csv', index = False)
+
+from sklearn import metrics
+
+print()
+print('Explained Variance Score:' , metrics.explained_variance_score(y_test, y_pred))
+print('R^2 Score:' , metrics.r2_score(y_test, y_pred))
+
+# Calculate the absolute errors
+errors = abs(y_pred - y_test)
+# Print out the mean absolute error (mae)
+print('Mean Absolute Error:', round(np.mean(errors), 2), 'degrees.')
+
+print()
